f624226ef33fef1cc7ae943378bf8d597075587f
Anna Pawlik <pawlikanna24@gmail.com>
2019-08-26 14:34:59 +0200

    Change script

--- js/App.js
+++ js/App.js
@@ -2,20 +2,39 @@
 
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': 'X-Client-Id',
-  'X-Auth-Token': 'X-Auth-Token'
+  'X-Client-Id': '4336',
+  'X-Auth-Token': '4fc5defc6a0b7c4692c4bdfbdd2c5a60'
 };
 
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+fetch(baseUrl + '/board', { headers: myHeaders })
+	.then(function(resp) {
+    	return resp.json();
+  	})
+  	.then(function(resp) {
+		setupColumns(resp.columns);
+	})
+	.catch(function(error) {
+		console.log(error);
+	});
+
+//Create columns
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.addColumn(col);
+		setupCards(col, column.cards);
+	});
 }
 
+//Create cards
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
+
+//Generate template
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
   	var element = document.createElement(basicElement || 'div');
@@ -24,22 +43,4 @@ function generateTemplate(name, data, basicElement) {
   	element.innerHTML = Mustache.render(template, data);
   
   	return element;
-}
-
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+};
